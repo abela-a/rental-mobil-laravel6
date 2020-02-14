66f55f25c46de475c2808f29d7e578e06665fe47
AbelArdhana <abelardhana96@gmail.com>
2020-02-14 17:16:50 +0800

    update sidebar

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -1,3 +1,30 @@
+@php
+if(Request::is('admin') || Request::is('karyawan') || Request::is('pelanggan')){
+$dashboard = 'active';
+}
+if(Request::is('*merk') || Request::is('*tipe') || Request::is('*mobil')){
+$kendaraan = 'active';
+}
+if(Request::is('*pelanggan') || Request::is('*karyawan') || Request::is('*role')){
+$akun = 'active';
+}
+if(Request::is('*pemesanan') || Request::is('*transaksi*')){
+$transaksi = 'active';
+}
+if(Request::is('*sopir')){
+$sopir = 'active';
+}
+if(Request::is('*merk') || Request::is('*tipe') || Request::is('*mobil')){
+$show_kendaraan = 'show';
+}
+if(Request::is('*pelanggan') || Request::is('*karyawan') || Request::is('*role')){
+$show_akun = 'show';
+}
+if(Request::is('*pemesanan') || Request::is('*transaksi')){
+$show_transaksi = 'show';
+}
+@endphp
+
 <nav id="sidebar">
   <div class="custom-menu">
     <button type="button" id="sidebarCollapse" class="btn btn-primary">
@@ -27,10 +54,13 @@
                 @php
                 if(Auth::user()->role_id === 1){
                 $role = 'Admin';
+                $role_route = 'admin';
                 } elseif(Auth::user()->role_id === 2){
                 $role = 'Karyawan';
+                $role_route = 'karyawan';
                 } else {
                 $role = 'Pelanggan';
+                $role_route = 'pelanggan';
                 }
                 @endphp
                 <span class="badge badge-info">{{ $role }}</span>
@@ -52,51 +82,27 @@
       </div>
     </div>
 
-    @php
-    if(Request::is('admin') || Request::is('karyawan') || Request::is('pelanggan')){
-    $dashboard = 'active';
-    }
-    if(Request::is('*merk') || Request::is('*tipe') || Request::is('*mobil')){
-    $kendaraan = 'active';
-    }
-    if(Request::is('*pelanggan') || Request::is('*karyawan') || Request::is('*role')){
-    $akun = 'active';
-    }
-    if(Request::is('*pemesanan') || Request::is('*transaksi*')){
-    $transaksi = 'active';
-    }
-    if(Request::is('*sopir')){
-    $sopir = 'active';
-    }
-    if(Request::is('*merk') || Request::is('*tipe') || Request::is('*mobil')){
-    $show_kendaraan = 'show';
-    }
-    if(Request::is('*pelanggan') || Request::is('*karyawan') || Request::is('*role')){
-    $show_akun = 'show';
-    }
-    if(Request::is('*pemesanan') || Request::is('*transaksi')){
-    $show_transaksi = 'show';
-    }
-    @endphp
-
     <ul class="list-unstyled components accordion px-4" id="accordion-sidebar">
       <li class="{{ $dashboard ?? '' }}">
-        <a href="{{ route('admin.dashboard') }}">
+        <a href="{{ route($role_route.'.dashboard') }}">
           <span class="fa fa-fw text-white fa-tachometer-alt mr-3"></span> Dashboard
         </a>
       </li>
+
+      @if (Auth::user()->role_id === 1 || Auth::user()->role_id === 2)
       <li class="{{ $kendaraan ?? '' }}">
         <a href="#kendaraan" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
           <span class="fa fa-fw text-white fa-car mr-3"></span>Kendaraan
         </a>
         <ul class="collapse list-unstyled {{ $show_kendaraan ?? '' }}" id="kendaraan" data-parent="#accordion-sidebar">
           <li>
-            <a href="{{ route('admin.merk') }}" class="{{ Request::is('*merk') ? 'text-active' : '' }}">Merk</a>
+            <a href="{{ route($role_route.'.merk') }}" class="{{ Request::is('*merk') ? 'text-active' : '' }}">Merk</a>
           </li>
           <li>
-            <a href="{{ route('admin.tipe') }}" class="{{ Request::is('*tipe') ? 'text-active' : '' }}">Tipe</a>
+            <a href="{{ route($role_route.'.tipe') }}" class="{{ Request::is('*tipe') ? 'text-active' : '' }}">Tipe</a>
           </li>
-          <li> <a href="{{ route('admin.mobil') }}" class="{{ Request::is('*mobil') ? 'text-active' : '' }}">Mobil</a>
+          <li> <a href="{{ route($role_route.'.mobil') }}"
+              class="{{ Request::is('*mobil') ? 'text-active' : '' }}">Mobil</a>
           </li>
         </ul>
       </li>
@@ -104,30 +110,37 @@
         <a href="#transaksi" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><span
             class="fa fa-fw text-white fa-dollar-sign mr-3"></span>Transaksi</a>
         <ul class="collapse list-unstyled {{ $show_transaksi ?? '' }}" id="transaksi" data-parent="#accordion-sidebar">
-          <li> <a href="{{ route('admin.pemesanan') }}"
+          <li> <a href="{{ route($role_route.'.pemesanan') }}"
               class="{{ Request::is('*pemesanan') ? 'text-active' : '' }}">Pemesanan</a> </li>
-          <li> <a href="{{ route('admin.transaksi') }}"
+          <li> <a href="{{ route($role_route.'.transaksi') }}"
               class="{{ Request::is('*transaksi') ? 'text-active' : '' }}">Transaksi</a> </li>
         </ul>
       </li>
       <li class="{{ $sopir ?? '' }}">
-        <a href="{{ route('admin.sopir') }}"><span class="fa fa-fw text-white fa-user-tie mr-3"></span>Data Sopir</a>
+        <a href="{{ route($role_route.'.sopir') }}"><span class="fa fa-fw text-white fa-user-tie mr-3"></span>Data
+          Sopir</a>
       </li>
       <li class="{{ $akun ?? '' }}">
         <a href="#akun" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><span
             class="fa fa-fw text-white fa-users mr-3"></span>Data Akun</a>
         <ul class="collapse {{ $show_akun ?? '' }} list-unstyled" id="akun" data-parent="#accordion-sidebar">
-          <li> <a href="{{ route('admin.pelanggan') }}"
+          <li> <a href="{{ route($role_route.'.pelanggan') }}"
               class="{{ Request::is('*pelanggan') ? 'text-active' : '' }}">Pelanggan</a> </li>
-          <li> <a href="{{ route('admin.karyawan') }}"
+          <li> <a href="{{ route($role_route.'.karyawan') }}"
               class="{{ Request::is('*karyawan') ? 'text-active' : '' }}">Karyawan</a> </li>
-          <li> <a href="{{ route('admin.role') }}" class="{{ Request::is('*role') ? 'text-active' : '' }}">Manajemen
+          <li> <a href="{{ route($role_route.'.role') }}"
+              class="{{ Request::is('*role') ? 'text-active' : '' }}">Manajemen
               Akses</a> </li>
         </ul>
       </li>
       <li>
         <a href="#"><span class="fa fa-fw text-white fa-print mr-3"></span>Laporan</a>
       </li>
+
+      @else
+
+      @endif
+
     </ul>
   </div>
 </nav>
