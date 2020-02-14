dbabb5ebb09cb7b48686c0c31de26a0c40791733
AbelArdhana <abelardhana96@gmail.com>
2020-02-14 19:45:43 +0800

    update middleware

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 
 class CheckAdmin
 {
@@ -20,12 +21,15 @@ class CheckAdmin
             return redirect()->route('login');
         }
         if (Auth::user()->role_id === 1) {
+            View::share(['role' => 'admin', 'role_name' => 'Admin']);
             return $next($request);
         }
         if (Auth::user()->role_id === 2) {
+            View::share(['role' => 'karyawan', 'role_name' => 'Karyawan']);
             return redirect()->route('karyawan.dashboard');
         }
         if (Auth::user()->role_id === 3) {
+            View::share(['role' => 'pelanggan', 'role_name' => 'Pelanggan']);
             return redirect()->route('pelanggan.dashboard');
         }
     }
